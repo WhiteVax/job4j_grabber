2fb22823b627f7cf6489257d4e9a11d3914b9539
WhiteVax <WhiteVax@gmail.com>
2022-08-25 23:06:23 +0300

    1. Quartz [#175122 #334940]

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS grabber(
+    id SERIAL PRIMARY KEY,
+    created_date TIMESTAMP
+);
+
--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -14,27 +17,51 @@ public class AlertRabbit {
     public static void main(String[] args) {
         try {
             var path = "rabbit.properties";
-            var scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            var job = newJob(Rabbit.class).build();
-            var times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(getProperties(path)
-                            .getProperty("rabbit.interval")))
-                    .repeatForever();
-            var trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException e) {
+            var property = getProperties(path);
+            Class.forName(property.getProperty("rabbit.driver"));
+            try (var connection = DriverManager.getConnection(
+                    property.getProperty("rabbit.url"),
+                    property.getProperty("rabbit.username"),
+                    property.getProperty("rabbit.password"))) {
+                var scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                var data = new JobDataMap();
+                data.put("connection", connection);
+                var job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                var times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(property.getProperty("rabbit.interval")))
+                        .repeatForever();
+                var trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(Integer.parseInt(property.getProperty("rabbit.sleep")));
+                scheduler.shutdown();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            try (var connection = (Connection) context.getJobDetail().getJobDataMap().get("connection")) {
+                try (var statement = connection.prepareStatement(
+                        "INSERT INTO grabber(created_date) VALUES (now())")) {
+                    statement.execute();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
