a264c30d58271b12851d3dd4135ee6d7f6b75781
WhiteVax <WhiteVax@gmail.com>
2022-08-27 00:04:08 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/parse/HabrCareerParse.java
+++ src/main/java/ru/job4j/parse/HabrCareerParse.java
@@ -1,16 +1,19 @@
 package ru.job4j.parse;
 
 import org.jsoup.Jsoup;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements DateTimeParser {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
+        var habr = new HabrCareerParse();
         var connection = Jsoup.connect(PAGE_LINK);
         var document = connection.get();
         var rows = document.select(".vacancy-card__inner");
@@ -19,9 +22,14 @@ public class HabrCareerParse {
             var linkElement = titleElement.child(0);
             var vacancyName = titleElement.text();
             var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            var date = row.select(".basic-date").attr("datetime");
+            var date = habr.parse(row.select(".basic-date").attr("datetime"));
             System.out.printf("%s %s %s%n", vacancyName, date, link);
         });
+    }
+
+    @Override
+    public  LocalDateTime parse(String parse) {
+        return  LocalDateTime.parse(parse.substring(0, 19));
     }
 }
 
