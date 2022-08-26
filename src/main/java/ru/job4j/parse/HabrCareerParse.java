7a3fed11013fabf943560e294a258b4f4949f3f4
WhiteVax <WhiteVax@gmail.com>
2022-08-27 00:20:33 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/parse/HabrCareerParse.java
+++ src/main/java/ru/job4j/parse/HabrCareerParse.java
@@ -10,21 +10,24 @@ public class HabrCareerParse implements DateTimeParser {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
         var habr = new HabrCareerParse();
-        var connection = Jsoup.connect(PAGE_LINK);
-        var document = connection.get();
-        var rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            var titleElement = row.select(".vacancy-card__title").first();
-            var linkElement = titleElement.child(0);
-            var vacancyName = titleElement.text();
-            var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            var date = habr.parse(row.select(".basic-date").attr("datetime"));
-            System.out.printf("%s %s %s%n", vacancyName, date, link);
-        });
+        for (int i = 1; i <= 5; i++) {
+            var connection = Jsoup.connect(String.format("%s%d", PAGE_LINK, i));
+            var document = connection.get();
+
+            var rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                var titleElement = row.select(".vacancy-card__title").first();
+                var linkElement = titleElement.child(0);
+                var vacancyName = titleElement.text();
+                var link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                var date = habr.parse(row.select(".basic-date").attr("datetime"));
+                System.out.printf("%s %s %s%n", vacancyName, date, link);
+            });
+        }
     }
 
     @Override
